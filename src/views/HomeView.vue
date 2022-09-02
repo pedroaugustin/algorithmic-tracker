32876162f7fe7151f2db0d362f9b5a2a370bd65f
unknown <predowtf@hotmail.com>
2022-09-01 23:44:29 -0300

    Fifths and ajustes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,6 +23,10 @@ import * as Tone from "tone";
 import * as Serialism from "total-serialism";
 
 const reverb = new Tone.Reverb().toDestination();
+const tremolo = new Tone.Tremolo(8, 1).toDestination().start();
+
+const Rand = Serialism.Stochastic;
+Rand.seed(666)
 
 const hihatSynth = new Tone.NoiseSynth({
     envelope: {
@@ -31,7 +35,7 @@ const hihatSynth = new Tone.NoiseSynth({
         sustain: 0.8,
         release: 0.001,
     },
-    volume: -15,
+    volume: -20,
 }).toDestination();
 
 const kickSynth = new Tone.MembraneSynth({
@@ -45,17 +49,18 @@ const kickSynth = new Tone.MembraneSynth({
         decay: 0.001,
         sustain: 1,
         release: 0.001,
-    }
+    },
+    volume: -6
 }).toDestination();
 
 const snareSynth = new Tone.NoiseSynth({
     envelope: {
-        attack: 0.001,
+        attack: 0.0001,
         decay: 0.001,
         sustain: 1,
         release: 0.15,
     },
-    volume: -10,
+    volume: -24,
 }).toDestination();
 
 const synth = new Tone.MonoSynth({
@@ -123,13 +128,48 @@ const synth2 = new Tone.MonoSynth({
         release: 0.8,
         releaseCurve: "exponential",
     },
-    volume: -10,
+    volume: -24,
 }).toDestination().connect(reverb);
+
+const synth3 = new Tone.MonoSynth({
+    oscillator: {
+        frequency: 440,
+        type: "sawtooth",
+    },
+    portamento: 0.05,
+    envelope: {
+        attack: 0.001,
+        attackCurve: "linear",
+        decay: 0.001,
+        decayCurve: "exponential",
+        sustain: 1,
+        release: 0.8,
+        releaseCurve: "exponential",
+    },
+    filter: {
+        Q: 1,
+        rollof: -12,
+        type: "lowpass",
+    },
+    filterEnvelope: {
+        attack: 0.001,
+        attackCurve: "linear",
+        baseFrequency: 400,
+        decay: 0.01,
+        decayCurve: "exponential",
+        exponent: 2,
+        octaves: 8,
+        sustain: 1,
+        release: 0.8,
+        releaseCurve: "exponential",
+    },
+    volume: -30,
+}).connect(tremolo).connect(reverb).toDestination();
 
 const bassSynth = new Tone.MonoSynth({
     oscillator: {
         frequency: 440,
-        type: "square",
+        type: "sawtooth",
     },
     envelope: {
         attack: 0.001,
@@ -137,7 +177,7 @@ const bassSynth = new Tone.MonoSynth({
         decay: 0.001,
         decayCurve: "exponential",
         sustain: 1,
-        release: 0.1,
+        release: 0.06,
         releaseCurve: "exponential",
     },
     filter: {
@@ -157,7 +197,7 @@ const bassSynth = new Tone.MonoSynth({
         release: 0.8,
         releaseCurve: "exponential",
     },
-    volume: -16,
+    volume: -20,
 }).toDestination();
 
 const Algo = Serialism.Algorithmic;
@@ -177,14 +217,26 @@ const SCALES = {
     MINOR_PENTATONIC: [0, 3, 5, 7, 10, 12, 15, 17, 19, 22, 24],
 };
 
-const PATTERNS = [[0, 0, 0, 0, 5, 5, 5, 5, 3, 3, 3, 3, 2, 2, 2, 2]];
+const PROGRESSIONS = [
+    [0, 0, 0, 0, 5, 5, 5, 5, 3, 3, 3, 3, 2, 2, 2, 2], // I-VI-IV-III
+    [0, 0, 0, 0, 3, 3, 3, 3, 0, 0, 0, 0, 6, 6, 6, 6],
+];
 
 const FIFTHS = [60, 67, 62, 69, 64, 71, 66, 61, 68, 63, 70, 65];
+
+const CHORDS = {
+    MAJOR: [0, 4, 7],
+    MAJOR_7: [0, 4, 7, 11],
+    MINOR: [0, 3, 7],
+    MINOR_7: [0, 3, 7, 11],
+    DIMINISHED: [0, 3, 6],
+}
 
 let numBassNotes = 4;
 let bassMelody = ["C4"];
 let synthMelody = ["C4"];
-let synth2Melody = ["C4"];
+let synth2Melody = [];
+let synth3Melody = [];
 let kickPattern = []
 let hihatPattern = []
 let snarePattern = []
@@ -209,23 +261,27 @@ const bassSeq = new Tone.Sequence((time, note) => {
 
 const synth2Part = new Tone.Part((time, value) => {
     synth2.triggerAttackRelease(value.note, "32n", time, value.velocity);
+})//.start("+0.1");
+
+const synth3Part = new Tone.Part((time, value) => {
+    synth3.triggerAttackRelease(value.note, value.duration, time, value.velocity);
 }).start("+0.1");
 
 const kickPart = new Tone.Part((time, value) => {
     kickSynth.triggerAttackRelease(value.note, "32n", time, value.velocity);
-}).start("+0.1");
+})//.start("+0.1");
 
 const hhPart = new Tone.Part((time, value) => {
     hihatSynth.triggerAttackRelease("64n", time, value.velocity);
-}).start("+0.1");
+})//.start("+0.1");
 
 const snarePart = new Tone.Part((time, value) => {
     snareSynth.triggerAttackRelease("32n", time, value.velocity);
-}).start("+0.1");
+})//.start("+0.1");
 
 
 function fillDrums() {
-    let seconds = Tone.Time("8n").toSeconds();
+    let seconds = Tone.Time("16n").toSeconds();
     kickPattern = [];
     hihatPattern = [];
     snarePattern = [];
@@ -235,13 +291,17 @@ function fillDrums() {
     hhPart.clear()
     snarePart.clear()
 
-    for (let i = 0; i < 128; i++) {
-        if (i % 4 == 0 || i == 0) {
+    for (let i = 0; i < 256; i++) {
+        if (i % 8 == 0 || i == 0) {
             kickPattern.push({ time: time, note: 20, velocity: 1 });
-        } else if (i % 8 == 2) {
+        } else if (i % 8 == 4) {
             snarePattern.push({ time: time, note: 60, velocity: 0.8 });
-        } else {
+        } else if (i % 2 == 0) {
             hihatPattern.push({ time: time, note: 50, velocity: 1 });
+        } else if (Rand.pick(1, [1, 2, 3, 4, 5]) == 1) {
+            snarePattern.push({ time: time, note: 20, velocity: 1 });
+        } else {
+            hihatPattern.push({ time: time, note: 20, velocity: 1 });
         }
 
         time = time + seconds;
@@ -259,18 +319,33 @@ function fillDrums() {
         snarePart.add(snarePattern[i]);
     }
 
-    console.log(hihatPattern)
-    console.log(kickPattern)
+    //console.log(hihatPattern)
+    //console.log(kickPattern)
 }
 
 function fillMelodyArray(melody) {
-    let seconds = Tone.Time("8n").toSeconds();
+    let seconds = Tone.Time("16n").toSeconds();
     let melodyArray = [];
     let time = 0;
 
     for (let i = 0; i < 128; i++) {
         if (melody[i] != 0) {
-            melodyArray.push({ time: time, note: Tone.Frequency(melody[i], "midi").toFrequency(), velocity: 0.5 });
+            melodyArray.push({ time: time, duration: i % 2 == 0 ? "4n" : "32n", note: Tone.Frequency(melody[i], "midi").toFrequency(), velocity: 1 });
+        }
+        time = time + seconds;
+    }
+
+    return melodyArray;
+}
+
+function fillMelody8nArray(melody) {
+    let seconds = Tone.Time("8n").toSeconds();
+    let melodyArray = [];
+    let time = 0;
+
+    for (let i = 0; i < 64; i++) {
+        if (melody[i] != 0) {
+            melodyArray.push({ time: time, duration: i % 2 == 0 ? "4n" : "32n", note: Tone.Frequency(melody[i], "midi").toFrequency(), velocity: 1 });
         }
         time = time + seconds;
     }
@@ -284,15 +359,15 @@ function generateBassMelody() {
     // randomizar esse
     let euclid = Algo.euclid(64, 54);
     let auxIndex = 0;
-    let bassPattern = 1;
+    let bassPattern = 0;
     let note;
 
     if (bassPattern == 0) {
         //euclidean
-        for (let i = 0; i < PATTERNS[0].length; i++) {
+        for (let i = 0; i < PROGRESSIONS[0].length; i++) {
             for (let j = 0; j < numBassNotes; j++) {
                 //randomizar pattern
-                note = (bassRoot + PATTERNS[0][i]) * euclid[auxIndex];
+                note = (bassRoot + PROGRESSIONS[0][i]) * euclid[auxIndex];
                 if (note == 0) {
                     bassMelody.push(0);
                 } else {
@@ -305,13 +380,13 @@ function generateBassMelody() {
         }
     } else {
         //1,2
-        for (let i = 0; i < PATTERNS[0].length; i++) {
+        for (let i = 0; i < PROGRESSIONS[0].length; i++) {
             for (let j = 0; j < numBassNotes; j++) {
                 //randomizar pattern
                 if (auxIndex % 2 == 0) {
-                    note = bassRoot + PATTERNS[0][i];
+                    note = bassRoot + PROGRESSIONS[1][i];
                 } else {
-                    note = bassRoot + PATTERNS[0][i] + 12;
+                    note = bassRoot + PROGRESSIONS[1][i] + 12;
                 }
                 note = Tone.Frequency(note, "midi").toFrequency();
                 bassMelody.push(note);
@@ -322,6 +397,24 @@ function generateBassMelody() {
     }
 
     console.log(bassMelody);
+}
+
+function generateSynthMelody() {
+    synthMelody = [];
+    let note = 0;
+    let octaveNote = 0;
+
+    for (let i = 0; i < SCALES["MINOR"].length; i++) {
+        note = synthRoot + SCALES["MINOR"][i];
+        octaveNote = note + 12
+        note = Tone.Frequency(note, "midi").toFrequency();
+        octaveNote = Tone.Frequency(octaveNote, "midi").toFrequency();
+        synthMelody.push(note);
+        synthMelody.push(octaveNote);
+    }
+
+    synthMelody.sort(function (a, b) { return a - b });
+    //console.log(synthMelody);
 }
 
 function generateSynth2Melody() {
@@ -335,12 +428,12 @@ function generateSynth2Melody() {
     let note;
 
     //euclidean
-    for (let i = 0; i < 128; i++) {
-        note =
-            (synthRoot + SCALES["MAJOR_PENTATONIC"][Math.floor(Math.random() * SCALES["MAJOR_PENTATONIC"].length)]) *
-            euclid[auxIndex];
-        synth2Notes.push(note);
-        auxIndex++;
+    for (let i = 0; i < PROGRESSIONS[1].length; i++) {
+        for (let j = 0; j < 8; j++) {
+            note = (synthRoot + PROGRESSIONS[1][i] + (SCALES["MINOR_PENTATONIC"][Math.floor(Math.random() * SCALES["MINOR_PENTATONIC"].length)])) * euclid[auxIndex];
+            synth2Notes.push(note);
+            auxIndex++;
+        }
     }
 
     synth2Melody = fillMelodyArray(synth2Notes);
@@ -349,25 +442,41 @@ function generateSynth2Melody() {
         synth2Part.add(synth2Melody[i]);
     }
 
-    console.log(synth2Melody);
+    //console.log(synth2Melody);
 }
 
-function generateSynthMelody() {
-    synthMelody = [];
-    let note = 0;
-    let octaveNote = 0;
+function generateSynth3Melody() {
+    synth3Melody = [];
+    synth3Part.clear();
 
-    for (let i = 0; i < SCALES["MAJOR_PENTATONIC"].length; i++) {
-        note = synthRoot + SCALES["MAJOR_PENTATONIC"][i];
-        octaveNote = note + 12
-        note = Tone.Frequency(note, "midi").toFrequency();
-        octaveNote = Tone.Frequency(octaveNote, "midi").toFrequency();
-        synthMelody.push(note);
-        synthMelody.push(octaveNote);
+    // randomizar esse
+    let auxIndex = 0;
+    let synth3Notes = [];
+    let note;
+
+    let ca = new Algo.Automaton();
+    // feed with 40 randomly generated values 0-1
+    ca.feed(Rand.coin(128));
+    ca.rule(122);
+    let gen = ca.next();
+
+    //euclidean
+    for (let i = 0; i < PROGRESSIONS[1].length; i++) {
+        for (let j = 0; j < 4; j++) {
+            note = (synthRoot + PROGRESSIONS[1][i] + (CHORDS["MINOR"][Math.floor(Math.random() * CHORDS["MINOR"].length)])) * gen[auxIndex];
+            synth3Notes.push(note);
+            auxIndex++;
+        }
     }
-    
-    synthMelody.sort(function(a, b){return a - b});
-    console.log(synthMelody);
+
+    console.log(synth3Notes);
+    synth3Melody = fillMelody8nArray(synth3Notes);
+
+    for (let i = 0; i < synth3Melody.length; i++) {
+        synth3Part.add(synth3Melody[i]);
+    }
+
+    //console.log("ae" , synth3Melody);
 }
 
 function setMelodies() {
@@ -388,6 +497,9 @@ function setup() {
     synth2Part.loop = true;
     synth2Part.loopStart = 0;
     synth2Part.loopEnd = "8m";
+    synth3Part.loop = true;
+    synth3Part.loopStart = 0;
+    synth3Part.loopEnd = "8m";
     kickPart.loop = true;
     kickPart.loopStart = 0;
     kickPart.loopEnd = "8m";
@@ -425,6 +537,7 @@ function generateNewPatterns() {
     generateBassMelody();
     generateSynthMelody();
     generateSynth2Melody();
+    generateSynth3Melody();
     setMelodies();
     fillDrums();
 }
@@ -465,7 +578,7 @@ export default {
         },
         changeNote() {
             Tone.start();
-            synth.triggerAttackRelease("C4", "16n");
+            synth3.triggerAttackRelease("C4", "2n");
         },
     },
 };
